bc969efd55d49c5b5347de296f4268f597affbb3
shinhong-gyu <shin02198@gmail.com>
2024-10-07 21:52:17 +0900

    [Feat] Item Base

--- Source/MetaCheeringRoom/SHK/HG_ItemBase.cpp
+++ Source/MetaCheeringRoom/SHK/HG_ItemBase.cpp
@@ -2,20 +2,27 @@
 
 
 #include "HG_ItemBase.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
 
 // Sets default values
 AHG_ItemBase::AHG_ItemBase()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComp"));
+	SetRootComponent(BoxComp);
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetupAttachment(RootComponent);
 }
 
 // Called when the game starts or when spawned
 void AHG_ItemBase::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
@@ -23,5 +30,44 @@ void AHG_ItemBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void AHG_ItemBase::UseItem()
+{
+}
+
+void AHG_ItemBase::SetItemName(FString Value)
+{
+	ItemName = Value;
+}
+
+FString AHG_ItemBase::GetItemName() const
+{
+	return ItemName;
+}
+
+void AHG_ItemBase::SetItemIcon(UTexture2D* Value)
+{
+	ItemIcon = Value;
+}
+
+UTexture2D* AHG_ItemBase::GetItemIcon() const
+{
+	return ItemIcon;
+}
+
+void AHG_ItemBase::SetQunatity(int32 Value)
+{
+	Qunatity = Value;
+}
+
+void AHG_ItemBase::AddQunatity(int32 Value)
+{
+	Qunatity += Value;
+}
+
+int32 AHG_ItemBase::GetQuantity() const
+{
+	return Qunatity;
 }
 
