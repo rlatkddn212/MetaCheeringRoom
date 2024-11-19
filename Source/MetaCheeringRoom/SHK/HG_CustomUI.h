d4623cdf8550d748941dd4589965d33135d28da9
shinhong-gyu <shin02198@gmail.com>
2024-11-19 16:53:31 +0900

    [Feat] 인벤토리 UI 끝, HUD UI 끝

---
+++ Source/MetaCheeringRoom/SHK/HG_CustomUI.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "HG_CustomUI.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class METACHEERINGROOM_API UHG_CustomUI : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+};
