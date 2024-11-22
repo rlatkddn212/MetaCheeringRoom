8bd1e7093d39a95c398c8675678d16b5f27ae187
Minibox <minibox724@gmail.com>
2024-11-22 15:27:51 +0900

    손 흔드는 애니메이션

---
+++ MetaCheering_LYJ.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Epic Games\UE_5.4\Engine\Binaries\Win64\UnrealEditor.exe" "C:\Projects\MetaCheeringRoom\MetaCheeringRoom.uproject" -game -ResX=1280 -ResY=720 -WinX=200 -WinY=300 -windowed -log
